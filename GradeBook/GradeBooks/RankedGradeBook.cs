bcbb6d2f77899d102587260c821fa400964404b1
EmotePhobia <kamilt2014@gmail.com>
2024-03-22 22:21:36 +0100

    Created Override RankedGradeBook's GetLetterGrade method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,5 +12,47 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+        
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires minimum 5 students");
+            }
+         
+            var grades = Students.OrderByDescending(s => s.AverageGrade).Select(s => s.AverageGrade).ToList();
+            int threshold20 = (int)Math.Ceiling(Students.Count * 0.2);
+            int threshold40 = (int)Math.Ceiling(Students.Count * 0.4);
+            int threshold60 = (int)Math.Ceiling(Students.Count * 0.6);
+            int threshold80 = (int)Math.Ceiling(Students.Count * 0.8);
+            int index = grades.FindIndex(g => g == averageGrade);
+
+            if(index < 0)
+            {
+                throw new InvalidOperationException("Student grade not found.");
+            }
+            
+            if(index < threshold20 )
+            {
+                return 'A';
+            }
+            else if (index < threshold40) 
+            {
+                return 'B';
+            }
+            else if(index < threshold60)
+            {
+                return 'C';
+            }
+            else if(index < threshold80)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+
+        }
     }
 }
